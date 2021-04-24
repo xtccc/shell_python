a51b594a58c726becd7e58556fa6f2919390c248
xtccc <2927497979@qq.com>
2021-04-24 20:58:36 +0800

    use python in shell script

---
+++ shell_python.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/bash
+test()
+{
+a=$(python3 << END
+if $errorcode == 0:
+    print('seabox init ok')
+else:
+    print(f'seabox init fail , errorcode is $errorcode')
+END
+)
+echo  $a
+}
+
+
+
+
+
+
+#errorcode=$(seabox init -a )
+errorcode='12'
+test
+errorcode='0'
+test
+
+
+
+
+
+
+input()
+{
+return_value=$(python3 << END
+for i in range(3):
+    print(f'ls',r'\n',f'rename foo foo0{i} foo?',r'\n')
+    
+END
+)
+echo -e $return_value
+}
+
+input
+
+arg_parse()
+{
+    value=$(python3 << END
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.'+r'\n')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator'+r'\n')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)'+r'\n')
+import sys
+sys.argv=['arg_parse']+list('$@'.split(' '))
+args = parser.parse_args()
+print(args.accumulate(args.integers))
+END
+)
+echo -e $value
+}
+arg_parse $@
+
+
+arg_parse '-h'
+arg_parse '--help'
+arg_parse '1 4 -h'
+arg_parse '1 4'
+arg_parse '1 4 78 --sum' 
